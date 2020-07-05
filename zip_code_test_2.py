8fc94736f6fc0bcc5450b55470a336ed1df5f6dd
jcpjm98 <66091160+jcpjm98@users.noreply.github.com>
2020-07-05 14:26:21 -0400

    Added changes to code

--- zip_code_test_2.py
+++ zip_code_test_2.py
@@ -4,8 +4,10 @@ zip_code = input("Please enter your five digit zip code: ")
 def zip_code_tester(zip_code):
     if len(zip_code) != 5:
         print("Incorrect response. Please try again.")
+        zip_code = input("Please enter your five digit zip code: ")
     elif zip_code.isalpha == True:
         print("Incorrect response. Please try again.")
+        zip_code = input("Please enter your five digit zip code: ")
     else:
         return zip_code
 
