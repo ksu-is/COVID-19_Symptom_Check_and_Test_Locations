1dd909d3b818f8d59c734454b93b59c4baa1d24b
mik3969 <mkhan54@students.kennesaw.edu>
2020-07-12 16:19:52 -0400

    Added a While true loop to attempt to fix bug in rejecting invalid responses.
    Will probably require further debugging

--- zip_code_test_2.py
+++ zip_code_test_2.py
@@ -1,15 +1,15 @@
 import webbrowser
-
 zip_code = input("Please enter your five digit zip code: ")
 def zip_code_tester(zip_code):
-    if len(zip_code) != 5:
-        print("Incorrect response. Please try again.")
-        zip_code = input("Please enter your five digit zip code: ")
-    elif zip_code.isalpha == True:
-        print("Incorrect response. Please try again.")
-        zip_code = input("Please enter your five digit zip code: ")
-    else:
-        return zip_code
+    while True: 
+        if len(zip_code) != 5:
+            print("Incorrect response. Please try again.")
+            zip_code = input("Please enter your five digit zip code: ")
+        elif zip_code.isalpha == True:
+            print("Incorrect response. Please try again.")
+            zip_code = input("Please enter your five digit zip code: ")
+        else:
+            return zip_code
 
 zip_code_tester(zip_code)
 
