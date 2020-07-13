fe8bc6cdb29a9c18bc638e5a0bdafc3bc8353e00
jcpjm98 <66091160+jcpjm98@users.noreply.github.com>
2020-07-13 17:05:34 -0400

    Debugged issue with 'dry cough' in symtpom2 and added the eigth symptom to our main code.
    Fixed an issue with dry cough prompt in symptom2 which made it so if a user failed at entering either 0 or 1 there answer would automatically be considered no. Also fixed some documentation issues and added the 8th symptom to our exam.

--- maincode.py
+++ maincode.py
@@ -1,13 +1,13 @@
 #Introductory greeting #
 print("Welcome to our COVID-19 Symptom Checker! We will ask you a series of questions regarding whether you are exhibiting any of the CDC's currently recognized list of COVID-19 Symptoms.")
 print("If you unfortunately are exhibiting symptoms of COVID-19, we will then provide you with resources to get tested locally.")
-# End Introduction #
+# End introductory greeting #
 
 # Symptoms Explaination #
 cdc_symptoms = ["Fever and/or chills, Coughing, Breathing difficulties, Fatigue, Muscle and/or body aches, Headache, Loss of taste and/or smell, Sore throat, Congestion, Runny nose, Nausea and/or vomitting, Diarrhea"]
 covid_symptoms = ", ".join(cdc_symptoms)
 print("Based on current CDC data, the currently recognized symptoms of COVID-19 are:", covid_symptoms)
-# End Explaination #
+# End symtoms explaination #
 
 # Establish repository for user responses #
 user_responses = []
@@ -16,7 +16,8 @@ user_symptoms = []
 
 # Symptoms Examination #
 print("We are about to ask you a series of medical questions. Please type '1' for 'Yes' or  '0' for 'No' for your responses.")
-## Begin symptom1. We will be using the same type of 'while True' functions to quiz users for each of the symptoms. This first one below will be used as a template for the reamining symptoms ##
+## Begin symptom1 ##
+### We will be using the same type of 'while True' functions to quiz users for each of the symptoms. This first one below will be used as a template for the reamining symptoms ###
 symptom1 = input("Within the last two weeks have you experienced a fever or bodily chills in any capacity? ")
 while True:
     if symptom1 == "1":
@@ -29,28 +30,32 @@ while True:
         if symptom1 != "1" or "0":
             print("Incorrect response. Please try again and enter '1' for 'Yes' or '0' for 'No'.")
             symptom1 = input("Within the last two weeks have you experienced a fever or bodily chills in any capacity? ")
- ## End symptom1 ##
+## End symptom1 ##
 
-## Begin symptom2. Notice how there is a secondary if function to determine if the user is experiencing a dry cough or just normal coughing bouts. ##
-## Because of the severity of this secondary symptom, we will end up appending it as two symptoms if a user has a dry cough ##
+## Begin symptom2 ##
+### Notice how there is a secondary if function to determine if the user is experiencing a dry cough or just normal coughing bouts. ###
+### Because of the severity of this secondary symptom, we will end up appending it as two symptoms if a user has a dry cough ###
 symptom2 = input("Within the last two weeks have you had any issues with coughing, especially dry coughing? ")
 while True:
     if symptom2 == "1":
         user_responses += ("1")
         user_symptoms.append("Coughing")
-        drycough = input("Have you experienced a dry cough? ")
-        if drycough == "1":
+        dry_cough = input("Have you experienced a dry cough within the last two weeks? ")
+        if dry_cough == "1":
             user_responses += ("1")
-            user_symptoms.append("Dry Cough")
+            user_symptoms.append("Dry cough")
+            break
+        elif dry_cough == "0":
             break
         else:
-            break
+            print("Incorrect response. Please try again and enter '1' for 'Yes' or '0' for 'No'.")
+            dry_cough = input("Have you experienced a dry cough within the last two weeks? ")
     elif symptom2 == "0":
         break
     else:
-        if symptom1 != "1" or "0":
+        if symptom2 != "1" or "0":
             print("Incorrect response. Please try again and enter '1' for 'Yes' or '0' for 'No'.")
-            symptom1 = input("Within the last two weeks have you experienced a fever or bodily chills in any capacity? ")
+            symptom2 = input("Within the last two weeks have you experienced a fever or bodily chills in any capacity? ")
 ## End symptom2 ##
 
 ## Begin symptom3 ##
@@ -127,6 +132,21 @@ while True:
             print("Incorrect response. Please try again and enter '1' for 'Yes' or '0' for 'No'.")
             symptom7 = input("Within the last two weeks have you experienced any diminishment of your sense of taste and/or smell? ")
 ## End symptom7 ##
+
+## Begin symptom8 ##
+symptom8 = input("Within the last two weeks have you experienced any noticeable throat soreness? ")
+while True:
+    if symptom8 == "1":
+        user_responses += ("1")
+        user_symptoms.append("Sore throat")
+        break
+    elif symptom8 == "0":
+        break
+    else:
+        if symptom8 != "1" or "0":
+            print("Incorrect response. Please try again and enter '1' for 'Yes' or '0' for 'No'.")
+            symptom8 = input("Within the last two weeks have you experienced any noticeable throat soreness? ")
+## End symptom8 ##
 
 
 
